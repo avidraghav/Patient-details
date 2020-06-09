fc8c5d683bdec5fd4f518d401701fb3c5993193b
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2020-06-09 13:10:53 +0530

    Added Login Screen with Update Profile, Update Password, Forget Password, Verify Phone, Verify Email etc features

--- app/src/main/java/com/example/medicalhistory/MainActivity.java
+++ app/src/main/java/com/example/medicalhistory/MainActivity.java
@@ -3,25 +3,50 @@ package com.example.medicalhistory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
-    TextInputLayout name, age, state, city, symptoms, address, id;
+    TextInputLayout symptoms;
+    AutoCompleteTextView symptoms1;
+    TextInputEditText name1, age1, state1, city1, address1, id1;
+    Button submitbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name=findViewById(R.id.patientname);
-        age=findViewById(R.id.age);
-        state=findViewById(R.id.State);
-        city=findViewById(R.id.City);
+        name1=findViewById(R.id.patientname1);
+        age1=findViewById(R.id.age1);
+        state1=findViewById(R.id.State1);
+        city1=findViewById(R.id.City1);
+         symptoms1=findViewById(R.id.Symptoms1);
+        address1=findViewById(R.id.Address1);
+        id1=findViewById(R.id.Phonenumber1);
+        submitbtn=findViewById(R.id.Submit_btn);
         symptoms=findViewById(R.id.Symptoms);
-        address=findViewById(R.id.Address);
-        id=findViewById(R.id.AadharId);
+
+        String[] listsymptoms=new String[]{
+                "Cough/Cold/Fever",
+                "Other"
+        };
+       ArrayAdapter<String> adapter=new ArrayAdapter<>(MainActivity.this,R.layout.symptomlist,listsymptoms);
+       symptoms1.setAdapter(adapter);
+
+
+        submitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, name1.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
